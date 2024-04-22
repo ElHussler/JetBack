76c79a2b711ca7d5eb324924c140bbbf0f626a3c
elhussler <animanic@hotmail.co.uk>
2024-04-22 16:03:53 +0100

    Intial project baseline commit

---
+++ JetBack/Plugin.cs
@@ -0,0 +1,22 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using JetBack.Patches;
+
+namespace JetBack
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class JetBackBase : BaseUnityPlugin
+    {
+        private const string modGUID = "Hussler.JetBack";
+        private const string modName = "JetBack";
+        private const string modVersion = "1.0.0";
+
+        void Awake()
+        {
+            //Add logging
+            
+            //Apply Patches
+        }
+    }
+}

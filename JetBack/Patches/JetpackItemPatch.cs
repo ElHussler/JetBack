cec1074983b5af14614a0cb92a37f69e8a4e1b2c
elhussler <animanic@hotmail.co.uk>
2024-04-22 16:15:06 +0100

    Update values

---
+++ JetBack/Patches/JetpackItemPatch.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+
+namespace JetBack.Patches
+{
+    [HarmonyPatch(typeof(JetpackItem))]
+    internal class JetpackItemPatch
+    {
+        [HarmonyPatch(nameof(JetpackItem.Update))]
+        [HarmonyPrefix]
+        static void UnnerfJetpackPatch(ref float ___jetpackAcceleration, ref float ___jetpackDeaccelleration)
+        {
+            ___jetpackAcceleration = 10f;
+            ___jetpackDeaccelleration = 45f;
+        }
+    }
+}
